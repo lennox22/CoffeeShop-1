0ebea92a26a336019ed9be56d165f689abe207a2
Lassiter Brown <lassiterbrown@quickenloans.com>
2021-05-12 00:53:55 -0400

    mvp reached

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult formreg(string firstname, string lastname, string email, string password, string birthdate)
+        {
+            ViewData["FirstName"] = firstname;
+            ViewData["LastName"] = lastname;
+            ViewData["Email"] = email;
+            ViewData["Password"] = password;
+            ViewData["Birthdate"] = birthdate;
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
